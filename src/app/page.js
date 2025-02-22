28bd3177cce8cef9b9d0b93c082162dadf0270fd
jayesh-2404 <jayeshpatil9028@gmail.com>
2025-02-22 19:19:49 +0530

    added chatbot

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@ import Home from "@/components/Landing/Home";
 import Footer from "@/components/Landing/Footer";
 import Header from "@/components/Landing/Header";
 import Contact from "@/components/Landing/Contact";
-
 export default function LandingPage() {
   return (
     <div>
       <Header />
-      <scheme/>
+      
       <Home/>
       <Contact/>
       <Footer />
+      {/* <Chat/> */}
     </div>
   );
 }
